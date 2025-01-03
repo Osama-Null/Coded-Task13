426c96ab49f4e02175a4f258acbd1dc86be3008a
Osama Ashkanani <osamaashkanani@gmail.com>
2025-01-03 14:41:11 +0300

    Update SQLQueryD13_T1.sql+Bonus

--- SQLQueryD13_T1.sql
+++ SQLQueryD13_T1.sql
@@ -29,7 +29,7 @@ insert into CodedT13.Courses values('History', 1),
 								   ('Russian', 2),
 								   ('Arabic', 3)
 go -- Q4
-select StudentName, CourseName from CodedT13.Student as s join CodedT13.Courses as c on s.StudentID = c.StID
+select distinct StudentName, CourseName from CodedT13.Student as s join CodedT13.Courses as c on s.StudentID = c.StID
 go -- Q5
 update CodedT13.Student set Email= 'vd@pm.ru' where StudentID = 1
 go -- Q6
@@ -39,6 +39,7 @@ go -- Q7
 alter table CodedT13.Student add Gender nvarchar(50)
 go -- Q8
 update CodedT13.Student set Gender = 'Male' where StudentID = 1
+go
 update CodedT13.Student set Gender = 'Female' where StudentID between 2 and 3
 go -- Q9
 select s.StudentName, COUNT(c.CourseID) from CodedT13.Student as s left join CodedT13.Courses as c on s.StudentID = c.StID group by s.StudentName;
@@ -59,11 +60,13 @@ select s.StudentID		as 'ID',
 from CodedT13.Student as s join CodedT13.Courses as c
 on s.StudentID = c.StID
 go -- Q16
-insert into CodedT13.Student values('Sam', 24, 'o@pm.kw'),
-								   ('Vi', 20, 'v@pm.ru')
-
-insert into CodedT13.Courses values('Sport', 5),
-								   ('Physics', 4)
+insert into CodedT13.Student values('Sam', 24, 'o@pm.kw', 'Male'),
+								   ('Vi', 20, 'v@pm.ru', 'Female')
+go
+insert into CodedT13.Courses values('Physics', 4),
+								   ('c#', 4),
+                                   ('Sport', 5)
+								   
 go -- Q17
 select StudentName as 'Name', Age from CodedT13.Student where Age = (select min(Age) from CodedT13.Student)
 go -- Q20
@@ -71,13 +74,75 @@ select CourseName from CodedT13.Courses order by CourseName asc
 go -- Q21
 create proc PrintNameNCourse as (
 	select StudentName, CourseName from CodedT13.Student as s join CodedT13.Courses as c on s.StudentID = c.StID
-) 
+)
+go
 exec PrintNameNCourse
 go -- Q22
 create proc PrintNameNCourseById as (
 	select StudentName, CourseName from CodedT13.Student as s join CodedT13.Courses as c on s.StudentID = c.StID
-) 
+)
+go
 exec PrintNameNCourse
-
+go
 select * from CodedT13.Courses
 select * from CodedT13.Student
+
+------------------------------------------------------------------------------
+go -- Bonus
+create table CodedT13.Student2(
+	StudentID int primary key identity(1, 1) not null,
+	--------------------------------------------------
+	StudentName nvarchar(50),
+	Age int,
+	Email nvarchar(100),
+	Gender nvarchar(50)
+)
+go
+create table CodedT13.Courses2(
+	CourseID int primary key identity(1, 1) not null, 
+	-------------------------------------------------
+	CourseName nvarchar(50)
+)
+go
+create table CodedT13.Enrollment2(
+	EnrollmentID int primary key identity(1, 1) not null, 
+	-------------------------------------------------
+	StudentID INT,
+    CourseID INT
+)
+go
+--========================================================================
+insert into CodedT13.Student2 values('Alice', 20, 'alice@example.com', 'Female'),
+									('Bob', 21, 'bob@example.com', 'Male'),
+									('Charlie', 22, 'charlie@example.com', 'Male'),
+									('Diana', 23, 'diana@example.com', 'Female'),
+									('Eve', 20, 'eve@example.com', 'Female')
+go
+insert into CodedT13.Courses2 values('Math'),
+								    ('Physics'),
+								    ('Chemistry'),
+								    ('Biology'),
+									('Computer Science')
+go
+insert into CodedT13.Enrollment2 values(1, 1),
+									   (1, 2),
+									   (2, 1),
+									   (2, 3),
+									   (3, 4),
+									   (3, 5),
+									   (4, 2),
+									   (4, 3),
+									   (5, 5)
+go
+select s2.StudentID		 as 'ID',
+	   s2.StudentName	 as 'Name',
+	   s2.Email          as 'Email',
+	   s2.Age            as 'Age',
+	   s2.Gender         as 'Gender',
+	   c2.CourseID		 as 'Course ID', 
+	   c2.CourseName	 as 'Course Name'
+from CodedT13.Student2 as s2 join CodedT13.Enrollment2 as e2 
+on s2.StudentID = e2.StudentID 
+
+join CodedT13.Courses2 as c2 
+on c2.CourseID = e2.CourseID
